9647857f18de46671b9b29db367a32e51b06cc04
syko240 <andrepost@hotmail.se>
2024-04-18 10:35:13 +0200

    sublists

--- P2/main.pl
+++ P2/main.pl
@@ -1,16 +1,38 @@
+% Compute Sublists
+
+sum([], 0).
+sum([H|T], Sum) :-
+    sum(T, Rest),
+    Sum is H + Rest.
+
 sublist(List, Start, End, Sublist) :-
-    findall(Elem, (nth1(Index, List, Elem), Index >= Start, Index =< End), Sublist).
+    Length is End - Start + 1,
+    drop(List, Start, Dropped),
+    take(Dropped, Length, Sublist).
+
+drop(List, N, Result) :-
+    length(Drop, N),
+    append(Drop, Result, List).
+
+take(List, N, Result) :-
+    length(Result, N),
+    append(Result, _, List).
 
 findSets(List, Sets) :-
     length(List, Length),
-    findall((Sublist, I, J), (between(1, Length, I), between(I, Length, J), sublist(List, I, J, Sublist)), Sets).
+    findall((Sum, I, J), (
+        between(0, Length, I),
+        between(I, Length, J),
+        sublist(List, I, J, Sublist),
+        sum(Sublist, Sum)
+        ), Sets).
 
 smallestKSets(K, List, Result) :-
     findSets(List, AllSets),
     length(AllSets, TotalSets),
     (TotalSets =:= 0 -> throw(error('list is empty'))
     ; K > TotalSets -> Result = AllSets
-    ; length(Result, K), append(Result, _, AllSets)
+    ; take(AllSets, K, Result)
     ).
 
-% smallestKSets(8, [3, 2, -4, 3, 2, -5, -2, 2, 3, -3, 2, -5, 6, -2, 2, 3], Result).
+% smallestKSets(3, [3, 2, -4, 3, 2, -5, -2, 2, 3, -3, 2, -5, 6, -2, 2, 3], Result).
