95b66ead58398d45494c74cde366e8bf8ccc857e
syko240 <andrepost@hotmail.se>
2024-04-21 21:31:43 +0200

    fix

--- P2/main.pl
+++ P2/main.pl
@@ -52,10 +52,10 @@ findSets(List, Sets) :-
         ), Sets).
 
 smallestKSets(K, List, Result) :-
-    findSets(List, AllSets),
-    length(AllSets, TotalSets),
-    (TotalSets =:= 0 -> throw(error('list is empty'))
-    ; mergeSort(AllSets, SortedSets),
+    length(List, N),
+    (N =:= 0 -> throw(error('list is empty'))
+    ; findSets(List, AllSets),
+    mergeSort(AllSets, SortedSets),
     take(SortedSets, K, Result)
     ).
 
@@ -75,6 +75,14 @@ printRows([(Sum, I, J) | T], List) :-
 
 % Testing
 
+testCase(15, [-1, 2, -3, 4, -5, 6, -7, 8, -9, 10, -11, 12, -13,
+    14, -15, 16, -17, 18, -19, 20, -21, 22, -23, 24, -25, 26,
+    -27, 28, -29, 30, -31, 32, -33, 34, -35, 36, -37, 38, -39,
+    40, -41, 42, -43, 44, -45, 46, -47, 48, -49, 50, -51, 52,
+    -53, 54, -55, 56, -57, 58, -59, 60, -61, 62, -63, 64, -65,
+    66, -67, 68, -69, 70, -71, 72, -73, 74, -75, 76, -77, 78,
+    -79, 80, -81, 82, -83, 84, -85, 86, -87, 88, -89, 90, -91,
+    92, -93, 94, -95, 96, -97, 98, -99, 100]).
 testCase(6, [24, -11, -34, 42, -24, 7, -19, 21]).
 testCase(8, [3, 2, -4, 3, 2, -5, -2, 2, 3, -3, 2, -5, 6, -2, 2, 3]).
 
